f54cc9633840ed387c621b308430cd04185b4822
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-11 19:02:32 +0100

    Finished activities 2 and 4

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,13 @@
 // Write Your JavaScript Code Here
+
+// Assign variables
+var personNAme = "Ethan";
+var pets = "0";
+var funFact = "I am a twin";
+
+// print variables
+console.log(
+  "My name is " + personNAme,
+  "I have " + pets + " pets. ",
+  "And a fun fact about me is " + funFact
+);
