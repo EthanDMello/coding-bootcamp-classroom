a20f0a94c7a88f01bb6b79a01556d99666ac8593
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-11 19:41:04 +0100

    Finished activity 10

--- 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,24 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var y = 50;
+var expression1 = x == y;
+var expression2 = x == y;
 
 // Write Your JavaScript Code Here
+
+var ex1;
+var ex2;
+
+if (expression1 == true) {
+  ex1 = "True ✅";
+} else {
+  ex1 = "False ❌";
+}
+
+if (expression2 == true) {
+  ex2 = "True ✅";
+} else {
+  ex2 = "False ❌";
+}
+
+console.log(ex1 + ex2);
