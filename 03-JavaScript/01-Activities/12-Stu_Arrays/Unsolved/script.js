922e7645bc5948fdfb3969e26f4f22b117a1ec5b
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-12 19:14:44 +0100

    Finished activity 12

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,12 @@
 // WRITE YOUR CODE HERE
+
+var studentNames = ["Ethan", "Baer", "Zohaib", "Akeem"];
+
+console.log(studentNames);
+
+for (let i = 0; i < studentNames.length; i++) {
+  console.log("Welcome to the class " + studentNames[i] + "!");
+  if (studentNames[0] != "Ethan") {
+    console.log(studentNames[0] + " is in class");
+  }
+}
