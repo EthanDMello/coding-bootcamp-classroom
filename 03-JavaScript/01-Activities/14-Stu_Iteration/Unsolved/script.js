5939613c0ad34c502e33e247a3f9978ea77e5526
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-12 19:32:04 +0100

    finished activity 14

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,15 @@
 //WRITE YOUR CODE BELOW
+
+var students = ["Ethan", "Baer", "Zohaib", "Akeem", "John"];
+
+console.log(students.length);
+
+for (let i = 0; i < 5; i++) {
+  console.log("Great to see you, " + students[i] + "!");
+}
+
+var i = 0;
+while (i < 5) {
+  console.log("Great to see you, " + students[i] + "!");
+  i++;
+}
