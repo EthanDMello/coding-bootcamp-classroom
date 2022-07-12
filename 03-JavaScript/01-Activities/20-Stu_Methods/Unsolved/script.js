e85c439eb9de9cc6732f37dfb8768ba732625dc3
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-12 20:42:27 +0100

    finished activity

--- 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,14 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+constellations.unshift("Canis Major");
+
+planets.pop();
+
+var galaxy = [constellations + planets];
+
+var newStar = star.toUpperCase();
+
+console.log(planets);
+console.log(galaxy);
+console.log(newStar);
