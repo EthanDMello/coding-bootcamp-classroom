1225af6655e812abebdb4a89e728914cd6d635a2
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-14 19:45:23 +0100

    finished activity 22 and 24

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,16 @@
 //WRITE YOUR CODE BELOW
+
+var customerOrder = {
+  name: "Coffee",
+  numOfSugar: 2,
+  ready: false,
+};
+
+console.log(customerOrder.name);
+console.log(customerOrder.numOfSugar);
+
+if (customerOrder.ready == true) {
+  console.log("Ready for pick-up");
+} else {
+  console.log("Still in order queue");
+}
