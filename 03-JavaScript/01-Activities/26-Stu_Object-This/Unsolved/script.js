b8f2d7b99e4ae866abc4882c2ebc2c588c5d099a
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-14 20:11:33 +0100

    activity 26 finished

--- 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,28 +1,32 @@
 // TODO: What does 'this' refer to?
+// where ever the statement is, in this case the whole window.
 console.log(this);
 
 // TODO: What does 'this' refer to?
-function helloThis() { 
-   console.log("Inside this function, this is " + this); 
-   }
+// inside the function
+function helloThis() {
+  console.log("Inside this function, this is " + this);
+}
 
-// TODO: What will this log? 
-var child = { 
-   age: 10,
-   ageTenYears: function() {
-       console.log(this.age + 10); 
-   }
+// TODO: What will this log?
+// 20
+var child = {
+  age: 10,
+  ageTenYears: function () {
+    console.log(this.age + 10);
+  },
 };
 
-// TODO: What will this log? 
-var investor = {  
+// TODO: What will this log?
+// 5000 * 1.15
+var investor = {
   name: "Cash Saver",
   investment: {
     initialInvestment: 5000,
-    investmentGrowth: function() {
-      console.log(this.initialInvestment * 1.15)
-    }
-   }   
+    investmentGrowth: function () {
+      console.log(this.initialInvestment * 1.15);
+    },
+  },
 };
 
 // TODO: After commenting, check your results!
