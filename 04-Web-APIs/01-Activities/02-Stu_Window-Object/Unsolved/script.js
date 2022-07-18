35d75bf73f536e195186b3fad6b6028503d94873
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-18 19:38:33 +0100

    finished activity 2 and 4

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,15 @@
 // TODO: What will the following line of code log?
+// all the functions in the javascript window
 console.log(window);
 
 // TODO: What will the following line of code log?
+// the whole html document
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+//the root elements in the HTML
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+// the head element in the html
 console.log(document.head);
-
