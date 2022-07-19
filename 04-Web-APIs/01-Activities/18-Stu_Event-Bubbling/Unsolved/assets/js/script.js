1aa334aaf1447bc991bd8889aa37dd5e7b18cf64
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-19 21:48:36 +0100

    finished activities 20

--- 04-Web-APIs/01-Activities/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
@@ -1,6 +1,8 @@
 // TODO: Which element is the following line of code selecting?
+// the element with carouselbox in the HTML document
 var carousel = document.querySelector(".carouselbox");
 // TODO: Which element is the following line of code selecting?
+//the button with the class next
 var next = carousel.querySelector(".next");
 var prev = carousel.querySelector(".prev");
 var index = 0;
@@ -10,16 +12,16 @@ var images = [
   "https://picsum.photos/300/200",
   "https://picsum.photos/300/201",
   "https://picsum.photos/300/202",
-  "https://picsum.photos/300/203"
+  "https://picsum.photos/300/203",
 ];
 
 carousel.style.backgroundImage = "url('https://picsum.photos/300/200')";
 
 function navigate(direction) {
   index = index + direction;
-  if (index < 0) { 
-    index = images.length - 1; 
-  } else if (index > images.length - 1) { 
+  if (index < 0) {
+    index = images.length - 1;
+  } else if (index > images.length - 1) {
     index = 0;
   }
   currentImage = images[index];
@@ -27,21 +29,26 @@ function navigate(direction) {
 }
 
 // TODO: Describe the functionality of the following event listener.
-carousel.addEventListener("click", function() {
+// waiting for a click on the carouselbox, then running the function
+carousel.addEventListener("click", function () {
   window.location.href = images[index];
 });
 
 // TODO: Describe the functionality of the following event listener.
-next.addEventListener("click", function(event) {
+// waiting for a click on the next button
+next.addEventListener("click", function (event) {
   // TODO: What is the purpose of the following line of code?
+  // stopping event bubbling
   event.stopPropagation();
 
   navigate(1);
 });
 
 // TODO: Describe the functionality of the following event listener.
-prev.addEventListener("click", function(event) {
-    // TODO: What would happen if we didn't add the following line of code?
+// waiting for a click on the prev button
+prev.addEventListener("click", function (event) {
+  // TODO: What would happen if we didn't add the following line of code?
+  // the parents in the DOM would activate too.
   event.stopPropagation();
 
   navigate(-1);
--- 04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -3,5 +3,11 @@ var container = document.querySelector(".container");
 container.addEventListener("click", function (event) {
   var element = event.target;
 
-  // TODO: Complete function
+  if (element.getAttribute("data-state") === "hidden") {
+    element.setAttribute("data-state", "visible");
+    element.textContent = element.getAttribute("data-number");
+  } else {
+    element.setAttribute("data-state", "hidden");
+    element.textContent = "";
+  }
 });
