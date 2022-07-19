3bca70366803067b1b6073cbc6d5be6c1fe0aeb5
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-19 20:55:34 +0100

    changes to activity 15 & 16

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,11 +1,24 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
+  document.querySelector("#status").innerHTML = "KEYDOWN Event";
 }
 
-function keyupAction() {
+elementKey = [];
+document.querySelector("#key").innerHTML = elementKey;
+function keyupAction(event) {
   document.querySelector("#status").innerHTML = "KEYUP Event";
+  var key = event.key.toLowerCase();
+  var alphabetNumericCharacters =
+    "abcdefghijklmnopqrstuvwxyz0123456789 .,'@".split("");
+  if (alphabetNumericCharacters.includes(key)) {
+    for (var i = 0; i < elementKey.length; i++) {
+      elementKey[i].textContent += event.key;
+      console.log(elementKey);
+    }
+  }
+  console.log(elementKey);
 }
 
 document.addEventListener("keyup", keyupAction);
 // TODO: Add Event Listener for "keydown" event
-
+document.addEventListener("keydown", keydownAction);
