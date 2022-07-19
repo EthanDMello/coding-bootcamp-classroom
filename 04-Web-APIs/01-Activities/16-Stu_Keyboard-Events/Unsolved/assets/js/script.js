7b54498da8d7ea82b71c52becf2985344dc4df8d
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-19 21:02:52 +0100

    finished activity 16

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,22 +1,12 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
   document.querySelector("#status").innerHTML = "KEYDOWN Event";
+  document.querySelector("#key").innerHTML = event.key;
+  document.querySelector("#code").innerHTML = event.code;
 }
 
-elementKey = [];
-document.querySelector("#key").innerHTML = elementKey;
 function keyupAction(event) {
   document.querySelector("#status").innerHTML = "KEYUP Event";
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters =
-    "abcdefghijklmnopqrstuvwxyz0123456789 .,'@".split("");
-  if (alphabetNumericCharacters.includes(key)) {
-    for (var i = 0; i < elementKey.length; i++) {
-      elementKey[i].textContent += event.key;
-      console.log(elementKey);
-    }
-  }
-  console.log(elementKey);
 }
 
 document.addEventListener("keyup", keyupAction);
