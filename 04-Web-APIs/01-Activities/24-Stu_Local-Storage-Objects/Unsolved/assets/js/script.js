8f3fdcadc0e3234ab059384f7d0cb720c1cf2b76
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-21 20:34:06 +0100

    finished activity 24

--- 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -3,12 +3,24 @@ var lastNameInput = document.querySelector("#last-name");
 var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 var signUpButton = document.querySelector("#sign-up");
+var submissionNum = 1;
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
-  
-  // TODO: Create user object from submissio
 
-  // TODO: Set new submission to local storage 
-  
+  // TODO: Create user object from submission
+  var submission = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  // TODO: Set new submission to local storage
+  var submissionNumStr = submissionNum.toString();
+  // console.log(submissionNumStr);
+  localStorage.setItem(submissionNumStr, JSON.stringify(submission));
+  submissionNum = parseInt(submissionNumStr, 10);
+  // console.log(submissionNum);
+  submissionNum++;
 });
