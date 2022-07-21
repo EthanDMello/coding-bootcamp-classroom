abf46168038e7288e425fcd835f83f81b02a2958
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-21 19:27:54 +0100

    finished activity 10

--- 04-Web-APIs/01-Activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -1,9 +1,9 @@
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
 
 var message =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var words = message.split(' ');
+  "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+var words = message.split(" ");
 
 function countdown() {
   var timeLeft = 5;
@@ -13,7 +13,14 @@ function countdown() {
     //
     // YOUR CODE HERE
     //
-  });
+    timerEl.textContent = timeLeft;
+    timeLeft--;
+    if (timeLeft == -1) {
+      clearInterval(timeInterval);
+      timerEl.textContent = "Go!";
+      displayMessage();
+    }
+  }, 1000);
 }
 
 // Displays the message one word at a time
@@ -31,7 +38,7 @@ function displayMessage() {
       mainEl.textContent = words[wordCount];
       wordCount++;
     }
-  }, 1000);
+  }, 400);
 }
 
 countdown();
