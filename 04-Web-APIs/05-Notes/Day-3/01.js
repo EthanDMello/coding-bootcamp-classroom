b2b4b69239261623184b43b0d71dad1a5a207b3e
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-21 18:55:11 +0100

    added max function

--- 04-Web-APIs/05-Notes/Day-3/01.js
+++ 04-Web-APIs/05-Notes/Day-3/01.js
@@ -2,14 +2,17 @@
 var integers = [1, 3, 6, 4, 2, 8, 12, 25, 9, 15, 20];
 var highNum = 0;
 
-// loop through array
-for (let i = 0; i < integers.length; i++) {
-  // check if current array item is higher
-  if (highNum < integers[i]) {
-    // if it is, change highNum variable
-    highNum = integers[i];
+// create function
+const max = () => {
+  // loop through array
+  for (let i = 0; i < integers.length; i++) {
+    // check if current array item is higher
+    if (highNum < integers[i]) {
+      // if it is, change highNum variable
+      highNum = integers[i];
+    }
   }
-}
+};
 
 // print output
 console.log("The highest number is " + highNum + ".");
