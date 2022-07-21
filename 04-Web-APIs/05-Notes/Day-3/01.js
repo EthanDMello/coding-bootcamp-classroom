4c9f66cb720e1efa691b52b6c7c025a86dc51e7e
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-21 18:50:52 +0100

    added notes folder

---
+++ 04-Web-APIs/05-Notes/Day-3/01.js
@@ -0,0 +1,15 @@
+// make initial array and comparative variable
+var integers = [1, 3, 6, 4, 2, 8, 12, 25, 9, 15, 20];
+var highNum = 0;
+
+// loop through array
+for (let i = 0; i < integers.length; i++) {
+  // check if current array item is higher
+  if (highNum < integers[i]) {
+    // if it is, change highNum variable
+    highNum = integers[i];
+  }
+}
+
+// print output
+console.log("The highest number is " + highNum + ".");
